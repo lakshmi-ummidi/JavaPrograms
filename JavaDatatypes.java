cbd22be6a61a94706459ac367218c67a2aa589bf
lakshmisairaghava <86336157+lakshmisairaghava@users.noreply.github.com>
2021-07-06 11:07:16 +0530

    JavaDatatypes

---
+++ JavaDatatypes.java
@@ -0,0 +1,43 @@
+/*JAVADATATPES
+Sample input : 
+5
+-150
+150000
+1500000000
+213333333333333333333333333333333333
+-100000000000000
+Sample output: 
+-150 can be fitted in:
+* short
+* int
+* long
+150000 can be fitted in:
+* int
+* long
+1500000000 can be fitted in:
+* int
+* long
+213333333333333333333333333333333333 can't be fitted anywhere.
+-100000000000000 can be fitted in:
+* long */
+import java.util.*;
+import java.io;
+class Solution{
+    public static void main(string []args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
+            try{
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)System.out.println("* byte");
+                if(x>=-32768 && x<=32767)System.out.println("* short");
+                if(x>=-2147483648 && x<=2147483648)System.out.ptintln("* int");
+                if(x>=-9223372036854775808L && x<=9223372036854775808L)System.out.println("* long");
+                }
+                catch(Exception e){
+                    System.out.println(sc.next()+" can't be fitted anywhere.");
+                }
+        }
+    }
+}
