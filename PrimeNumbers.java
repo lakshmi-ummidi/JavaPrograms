2813c1faa2780a68ac2effae01152cf6bc2e029a
lakshmisairaghava <86336157+lakshmisairaghava@users.noreply.github.com>
2021-07-06 11:06:15 +0530

    PrimeNumbers

---
+++ PrimeNumbers.java
@@ -0,0 +1,44 @@
+  
+/*Lab 2.1) Write a Java program to select all the prime numbers within the range of 1 
+
+to100. */
+
+public class PrimeNumbers {
+
+	public static void main(String[] args) {		int i,j,n=0,temp=0;
+
+		System.out.print("Prime numbers within the range of 1 to 100 are: ");
+
+		for(i=1;i<=100;i++)
+
+		{
+
+			for(j=1;j<=i;j++)
+
+			{
+
+				if(i%j==0)
+
+				{
+
+					temp++;
+
+				}
+
+			}
+
+			if(temp==2)
+
+			{
+
+				System.out.print(i+" ");
+
+			}
+
+				temp=0;
+
+		}
+
+	}
+
+}
