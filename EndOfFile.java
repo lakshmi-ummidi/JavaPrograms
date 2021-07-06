fcde805d488efc0b163dc6e164e662e97e340aaf
lakshmisairaghava <86336157+lakshmisairaghava@users.noreply.github.com>
2021-07-06 11:17:30 +0530

    EndOfFile

--- EndOfFile.java
+++ EndOfFile.java
@@ -1,3 +1,11 @@
+/* Sample Iput: 
+Hello world
+I am a file
+Read me until end-of-file.
+Sample Output:
+1 Hello world
+2 I am a file
+3 Read me until end-of-file.*/
 import java.io.*;
 import java.util.*;
 
